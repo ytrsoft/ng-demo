c919f2ee8221343038f30ea451a46f2ef4a12591
ytrsoft <1654486219@qq.com>
2025-04-11 21:34:43 +0800

    forms

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../shared.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -10,5 +11,22 @@ import { SharedModule } from '../shared.module';
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
+  loginForm: FormGroup
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('登录成功', this.loginForm.value)
+    } else {
+      console.log('表单无效')
+      // 触发验证
+      this.loginForm.markAllAsTouched()
+    }
+  }
 }
